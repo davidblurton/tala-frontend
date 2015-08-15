e27f5be96ebddb464faa83c91651edfd969f1642
David Blurton <david.blurton@gmail.com>
2015-08-15 14:36:20 +0000

    New result format

--- app/routes/verb/word.js
+++ app/routes/verb/word.js
@@ -4,7 +4,7 @@ import config from '../../config/environment';
 
 export default Ember.Route.extend({
   model(params) {
-    var url = `${config.api}/verb/${params.word}`;
+    var url = `${config.api}/verb/${params.word}?lang=en`;
     return ajax(url);
   }
 });
